05024dc9ceee2274a01c3536aa40daef77c55669
Tanishq Palkhe <115395213+tanishq1232e2q@users.noreply.github.com>
2025-08-31 10:05:43 +0530

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Signup from './components/Signup'
 import Login from './components/Login'
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Welcome from './components/Welcome'
 import dd from "../public/top.png"
 import col from "../public/right-column.png"
